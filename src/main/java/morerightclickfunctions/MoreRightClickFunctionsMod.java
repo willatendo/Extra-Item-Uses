1edc8032304f57f03b342c83d8c487b1c755bf9d
Willatendo <williamjswindler@gmail.com>
2022-07-11 22:34:00 -0600

    setup

--- src/main/java/morerightclickfunctions/MoreRightClickFunctionsMod.java
+++ src/main/java/morerightclickfunctions/MoreRightClickFunctionsMod.java
@@ -2,20 +2,17 @@ package morerightclickfunctions;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
-import morerightclickfunctions.server.MoreRightClickFunctionsConfig;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
-@Mod(MoreRightClickFunctionsMod.ID)
-public class MoreRightClickFunctionsMod {
+public class MoreRightClickFunctionsMod implements ModInitializer {
 	public static final String ID = "morerightclickfunctions";
 	public static final Logger LOGGER = LogManager.getLogger(ID);
 
-	public MoreRightClickFunctionsMod() {
-		ModLoadingContext.get().registerConfig(Type.COMMON, MoreRightClickFunctionsConfig.commonSpec);
+	@Override
+	public void onInitialize(ModContainer modContainer) {
 	}
 
 	public static ResourceLocation rL(String path) {
