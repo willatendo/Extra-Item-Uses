847a5b5ee08dd962a1c67240ceb48ef5ae36e860
Willatendo <williamjswindler@gmail.com>
2022-07-12 10:42:37 -0600

    fabric 1.0.0

--- src/main/java/morerightclickfunctions/MoreRightClickFunctionsMod.java
+++ src/main/java/morerightclickfunctions/MoreRightClickFunctionsMod.java
@@ -3,8 +3,14 @@ package morerightclickfunctions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import morerightclickfunctions.server.MoreRightClickFunctionsConfig;
+import morerightclickfunctions.server.MoreRightClickFunctionsMaps;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 public class MoreRightClickFunctionsMod implements ModInitializer {
 	public static final String ID = "morerightclickfunctions";
@@ -12,6 +18,41 @@ public class MoreRightClickFunctionsMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModLoadingContext.registerConfig(ID, Type.COMMON, MoreRightClickFunctionsConfig.commonSpec);
+
+		for (int i = 0; i < MoreRightClickFunctionsConfig.COMMON_CONFIG.crackables_input.get().size(); i++) {
+			Block input = Registry.BLOCK.get(new ResourceLocation(MoreRightClickFunctionsConfig.COMMON_CONFIG.crackables_input.get().get(i)));
+			Block output = Registry.BLOCK.get(new ResourceLocation(MoreRightClickFunctionsConfig.COMMON_CONFIG.crackables_output.get().get(i)));
+			if (input == null) {
+				MoreRightClickFunctionsMod.LOGGER.debug("Warning! Crackables is broken and the game will crash!");
+			}
+			if (output == null) {
+				MoreRightClickFunctionsMod.LOGGER.debug("Warning! Crackables is broken and the game will crash!");
+			}
+			MoreRightClickFunctionsMaps.CRACKABLES.put(input, output);
+		}
+		for (int i = 0; i < MoreRightClickFunctionsConfig.COMMON_CONFIG.grindables_input.get().size(); i++) {
+			Block input = Registry.BLOCK.get(new ResourceLocation(MoreRightClickFunctionsConfig.COMMON_CONFIG.grindables_input.get().get(i)));
+			Block output = Registry.BLOCK.get(new ResourceLocation(MoreRightClickFunctionsConfig.COMMON_CONFIG.grindables_output.get().get(i)));
+			if (input == null) {
+				MoreRightClickFunctionsMod.LOGGER.debug("Warning! Grindables is broken and the game will crash!");
+			}
+			if (output == null) {
+				MoreRightClickFunctionsMod.LOGGER.debug("Warning! Grindables is broken and the game will crash!");
+			}
+			MoreRightClickFunctionsMaps.GRINDABLES.put(input, output);
+		}
+		for (int i = 0; i < MoreRightClickFunctionsConfig.COMMON_CONFIG.chiselables_input.get().size(); i++) {
+			Block input = Registry.BLOCK.get(new ResourceLocation(MoreRightClickFunctionsConfig.COMMON_CONFIG.chiselables_input.get().get(i)));
+			Block output = Registry.BLOCK.get(new ResourceLocation(MoreRightClickFunctionsConfig.COMMON_CONFIG.chiselables_output.get().get(i)));
+			if (input == null) {
+				MoreRightClickFunctionsMod.LOGGER.debug("Warning! Chiselables is broken and the game will crash!");
+			}
+			if (output == null) {
+				MoreRightClickFunctionsMod.LOGGER.debug("Warning! Chiselables is broken and the game will crash!");
+			}
+			MoreRightClickFunctionsMaps.CHISELABLES.put(input, output);
+		}
 	}
 
 	public static ResourceLocation rL(String path) {
---
+++ src/main/java/morerightclickfunctions/server/jei/category/GrindablesCategory.java
@@ -0,0 +1,52 @@
+package morerightclickfunctions.server.jei.category;
+
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import morerightclickfunctions.server.jei.MoreRightClickFunctionsJeiConstants;
+import morerightclickfunctions.server.jei.recipe.GrindablesRecipe;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public class GrindablesCategory implements IRecipeCategory<GrindablesRecipe> {
+	private final IDrawable background;
+	private final IDrawable icon;
+
+	public GrindablesCategory(IGuiHelper guiHelper) {
+		this.background = guiHelper.createBlankDrawable(80, 40);
+		this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, Items.DIAMOND_SHOVEL.getDefaultInstance());
+	}
+
+	@Override
+	public RecipeType<GrindablesRecipe> getRecipeType() {
+		return MoreRightClickFunctionsJeiConstants.GRINDABLES;
+	}
+
+	@Override
+	public Component getTitle() {
+		return Component.translatable("jei.morerightclickfunctions.grindables");
+	}
+
+	@Override
+	public IDrawable getBackground() {
+		return this.background;
+	}
+
+	@Override
+	public IDrawable getIcon() {
+		return this.icon;
+	}
+
+	@Override
+	public void setRecipe(IRecipeLayoutBuilder builder, GrindablesRecipe recipe, IFocusGroup focuses) {
+		builder.addSlot(RecipeIngredientRole.INPUT, 5, 10).addIngredients(Ingredient.of(recipe.input().asItem()));
+		builder.addSlot(RecipeIngredientRole.CATALYST, 32, 10).addIngredients(Ingredient.of(Items.DIAMOND_SHOVEL));
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 60, 10).addIngredients(Ingredient.of(recipe.output().asItem()));
+	}
+}
