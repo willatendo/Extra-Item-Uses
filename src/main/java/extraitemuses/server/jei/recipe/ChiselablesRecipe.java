cd285ca88c25f17191de2ac3ef7059c6ad24834f
Willatendo <williamjswindler@gmail.com>
2023-09-23 21:15:55 -0600

    oops

--- src/main/java/extraitemuses/server/jei/recipe/ChiselablesRecipe.java
+++
@@ -1,6 +0,0 @@
-package extraitemuses.server.jei.recipe;
-
-import net.minecraft.world.level.block.Block;
-
-public record ChiselablesRecipe(Block input, Block output) {
-}
--- src/main/java/extraitemuses/server/jei/recipe/GrindablesRecipe.java
+++
@@ -1,6 +0,0 @@
-package extraitemuses.server.jei.recipe;
-
-import net.minecraft.world.level.block.Block;
-
-public record GrindablesRecipe(Block input, Block output) {
-}
